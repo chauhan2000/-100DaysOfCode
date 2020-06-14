fbd543905c07ead785192df0e2b583f6929884ac
Rachit Chauhan <36044440+chauhan2000@users.noreply.github.com>
2020-06-14 12:56:23 +0530

    Encapsulation in Java

---
+++ Encapsulation.java
@@ -0,0 +1,15 @@
+Class data
+{
+private String name;
+Public String getname()
+{
+return name;
+}
+Class example
+{
+public static void main(String args[])
+{
+data obj = new data();
+obj.setname("Rachit");
+}
+}
